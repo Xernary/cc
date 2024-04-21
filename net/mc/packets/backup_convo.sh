fb702ba36528213190ef4f17f1ee3a708e1db8e8
Xernary <nicolalggiuffrida@gmail.com>
2024-04-21 14:33:04 +0200

    idk

---
+++ net/mc/packets/backup_convo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp $HOME/cyberchallenge/net/mc/packets/convos/client_convo.txt $HOME/cyberchallenge/net/mc/packets/convos/client_convo_backup.txt
---
+++ net/mc/packets/copy_to_convo.sh
@@ -0,0 +1,1 @@
+xxd -p -c 10000000 $HOME/cyberchallenge/net/mc/packets/unc/$1.unc >> $HOME/cyberchallenge/net/mc/packets/convos/client_convo.txt
